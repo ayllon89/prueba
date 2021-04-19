61141100045c576ecb0042a456eeadc67d00fdd4
Luis <ayllon_luis@hotmail.com>
2021-04-19 00:24:07 -0300

    prueba luis ultima

---
+++ prueba2/src/proceso.c
@@ -0,0 +1,21 @@
+/*
+ ============================================================================
+ Name        : proceso.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello Wosaooooosasrld");
+
+    return 0;
+}
